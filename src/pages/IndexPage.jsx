bbde4ebb7d2c59cba31b0ec594c36f4d0a3cd1d2
samchi <seyoung0314@gmail.com>
2025-02-11 11:14:48 +0900

    scss 세팅

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import styles from './IndexPage.module.scss';
 
 const IndexPage = () => {
   return (
-    <div>IndexPage</div>
-  )
-}
+    <div className={styles.home}>
+      <h1 className={styles.title}>개발자의 기술 블로그</h1>
+      <p className={styles.subtitle}>React와 관련된 기술들을 공유합니다.</p>
 
-export default IndexPage
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/blog'>Blog</a>
+        </li>
+        <li>
+          <a href='/about'>About</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default IndexPage;
