dfb4acf4afaf21243cdf7630224f0ac3ae3b3142
samchi <seyoung0314@gmail.com>
2025-02-11 11:58:01 +0900

    navLink 사용하기

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,14 +1,22 @@
-import { Link } from 'react-router-dom';
-import styles from './MainNav.module.scss';
+import { Link, NavLink } from "react-router-dom";
+import styles from "./MainNav.module.scss";
 
 const MainNav = () => {
+  const activeLink = ({isActive}) => `${styles.navLink} ${isActive ? styles.active : ''}`;
+
   return (
     <nav className={styles.nav}>
-      <Link className={styles.navLink} to='/'>Home</Link>
-      <Link className={styles.navLink} to='/blog'>Blog</Link>
-      <Link className={styles.navLink} to='/about'>About</Link>
+      <NavLink className={activeLink} to="/">
+        Home
+      </NavLink>
+      <NavLink className={activeLink} to="/blog">
+        Blog
+      </NavLink>
+      <NavLink className={activeLink} to="/about">
+        About
+      </NavLink>
     </nav>
   );
-}
+};
 
-export default MainNav
+export default MainNav;
