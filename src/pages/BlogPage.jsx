4011694b6d37ed09098ec73eb64ab543d7d3830a
samchi <seyoung0314@gmail.com>
2025-02-12 09:35:42 +0900

    블로그페이지만들기

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import MainNav from '../components/MainNav';
+import MainNav from "../components/MainNav";
+import styles from "./BlogPage.module.scss";
+import PostCard from "../components/PostCard";
+import { posts } from "../assets/dummy-data";
 
 const BlogPage = () => {
   return (
-    <>
-      <h1>BlogPage</h1>
-    </>
+    <div className={styles.blog}>
+      <div className={styles.grid}>
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
   );
 };
 
